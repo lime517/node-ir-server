d66959983f3d9a83a330b162b0b810485b186407
Joseph Greve <limejoe517@gmail.com>
2022-04-13 23:45:56 -0400

    Timing Debuggin

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ class irControllerSystem {
   rawInput(keycode) {
     // First, Check if this is a fast duplicate. 35ms is impossibly fast for a human to double-tap.
     if (Date.now() < this.lastNewKeypress + 35) {
-        console.log('skipping');
+        console.log('skipping', Date.now(), this.lastNewKeypress);
       return; // do nothing. Just straight up ignore this.
     } else {
-        console.log('happening');
+        console.log('happening', Date.now(), this.lastNewKeypress);
     }
 
     // Otherwise, carry on.
