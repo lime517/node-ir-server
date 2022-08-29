bf8adb0767860050a227536b97293d0a3fd333a0
Joseph Greve <limejoe517@gmail.com>
2022-08-28 23:30:44 -0400

    Add error handling so that it doesn't panic

--- app.js
+++ app.js
@@ -142,12 +142,26 @@ class irControllerSystem {
   }
 
   apiRequest(endpoint, keycode, callback) {
-    let request = axios.get(this.apiBase + endpoint);
+    // Make the Axios HTTP Request, now with error handling!
+    let request = axios.get(this.apiBase + endpoint).catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
     return request;
-  }
-
-  apiResponseCallback(keycode) {
-    console.log("Successful request");
   }
 
   mute() {
